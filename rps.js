744ba8f8f50e23352362474a4001d68805830970
Rodlau98 <lautirod12@gmail.com>
2022-09-01 22:50:55 -0300

    Add game() function and change the return of the playRound() function

--- rps.js
+++ rps.js
@@ -10,7 +10,6 @@ function getComputerChoice(){
     }
 }
 
-const playerChoice = prompt("Choose Rock, Paper or Scissors");
 //This function takes the player and the computer selections and plays a round of rock, paper, scissors.
 function playRound(playerSelection, computerSelection){
     let p = playerSelection.toLowerCase();
@@ -22,16 +21,56 @@ function playRound(playerSelection, computerSelection){
     console.log("The computer chose: " + firstUpperC);
 
     if (p !== "rock" && p !== "paper" && p !== "scissors"){
-        return `${p} is not a valid entry! Please refresh and try again.`
+        console.log(`${p} is not a valid entry!`);
+        return 0;
     } else
     if (p === c) {
-        return "We have a tie!";
+        console.log("We have a tie!");
+        return 1;
     } else if (p === "rock" && c === "paper" || p === "scissors" && c === "rock" || p === "paper" && c === "scissors"){
-        return "You lose! " + firstUpperC + " beats " + firstUpperP + ". Better luck next time!";
+        console.log("You lose! " + firstUpperC + " beats " + firstUpperP + ". Better luck next time!");
+        return 2;
     } else {
-        return "You win! " + firstUpperP + " beats " + firstUpperC + ". Good job!";
+        console.log("You win! " + firstUpperP + " beats " + firstUpperC + ". Good job!");
+        return 3;
     }
     
 }
 
-console.log(playRound(playerChoice, getComputerChoice()));
+//This functions plays the game 5 times and then displays the results at the end
+function game(){
+    let playerWin = 0;
+    let computerWin = 0;
+    let tie = 0;
+    let error = 0;
+    let result;
+
+    for (let i = 0; i < 5; i++) {
+        result = playRound(prompt("Choose Rock, Paper or Scissors"), getComputerChoice());
+        if (result === 0) {
+            error++;
+        } else if(result === 1){
+            tie++;
+        } else if (result === 2){
+            computerWin++;
+        } else if (result === 3){
+            playerWin++;
+        } 
+    }
+    
+    console.log(`There were a total of ${playerWin} wins, ${computerWin} losses and ${tie} ties.`);
+    
+    if(error > 0){
+        console.log(`Ups! There were ${error} misspelling(s). We are not counting those.`);
+    }
+    
+    if(playerWin > computerWin){
+        console.log("Congratulations! You won!");
+    } else if (playerWin < computerWin){
+        console.log("Too bad, it seems like you lost...");
+    } else {
+        console.log("It's a tie!");
+    }  
+}
+
+game();
