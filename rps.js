9ba8e38cb45eec06846cce1e4c04f0231eb684c9
Rodlau98 <lautirod12@gmail.com>
2022-09-01 21:52:41 -0300

    Add playRound() function to rps.js

--- rps.js
+++ rps.js
@@ -9,3 +9,29 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+
+const playerChoice = prompt("Choose Rock, Paper or Scissors");
+//This function takes the player and the computer selections and plays a round of rock, paper, scissors.
+function playRound(playerSelection, computerSelection){
+    let p = playerSelection.toLowerCase();
+    let c = computerSelection.toLowerCase();
+    let firstUpperC = c.charAt(0).toUpperCase() + c.slice(1);
+    let firstUpperP = p.charAt(0).toUpperCase() + p.slice(1);
+    
+    console.log("You chose: " + firstUpperP);
+    console.log("The computer chose: " + firstUpperC);
+
+    if (p !== "rock" && p !== "paper" && p !== "scissors"){
+        return `${p} is not a valid entry! Please refresh and try again.`
+    } else
+    if (p === c) {
+        return "We have a tie!";
+    } else if (p === "rock" && c === "paper" || p === "scissors" && c === "rock" || p === "paper" && c === "scissors"){
+        return "You lose! " + firstUpperC + " beats " + firstUpperP + ". Better luck next time!";
+    } else {
+        return "You win! " + firstUpperP + " beats " + firstUpperC + ". Good job!";
+    }
+    
+}
+
+console.log(playRound(playerChoice, getComputerChoice()));
